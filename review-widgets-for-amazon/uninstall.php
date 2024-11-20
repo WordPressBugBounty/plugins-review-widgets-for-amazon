fb1a2ec88eff538ceb716351f6c0e889b489052e
Sean Murphy <sean@iamseanmurphy.com>
2024-11-20 11:23:10 +0000

    12.4.4

--- review-widgets-for-amazon/uninstall.php
+++ review-widgets-for-amazon/uninstall.php
@@ -3,6 +3,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 die;
 }
 require_once plugin_dir_path( __FILE__ ) . 'trustindex-plugin.class.php';
-$trustindex_pm_amazon = new TrustindexPlugin_amazon("amazon", __FILE__, "12.4.3", "Widgets for Amazon Reviews", "Amazon");
+$trustindex_pm_amazon = new TrustindexPlugin_amazon("amazon", __FILE__, "12.4.4", "Widgets for Amazon Reviews", "Amazon");
 $trustindex_pm_amazon->uninstall();
 ?>
